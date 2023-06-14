9e4a5a035c288c94b906cb81497b2146b96fe969
alexistincho <86344432+alexistincho@users.noreply.github.com>
2023-06-14 16:06:48 -0300

    Add files via upload

---
+++ trabajo De todos/src/validacionFormulario.js
@@ -0,0 +1,28 @@
+function validarFormulario()
+{
+  'use strict'
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+	
+}
+
+function iniciar()
+{
+	validarFormulario();
+
+}
+
+window.addEventListener("load", iniciar);
