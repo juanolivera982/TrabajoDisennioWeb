72395475f942efbabb61ed67201919435cba2cae
Javierochoa159 <118292286+Javierochoa159@users.noreply.github.com>
2023-06-15 19:02:17 -0300

    Add files via upload

--- src/contactos.js
+++ src/contactos.js
@@ -1,5 +1,3 @@
-const formulario=document.getElementById("formularioContacto");
-
 function iniciar()
 {
 	var boton = document.getElementById("botonContacto");
@@ -9,22 +7,17 @@ function iniciar()
 function enviarFormulario()
 {
 	var form = document.querySelector("form[name='formularioContacto']");
-
-	if(form.checkValidity()){
-		formulario.addEventListener('submit',(event)=>{
-            //event.preventDefault();
-            //const datos=new FormData(event.target);
-            //const datosCompletos=Object.fromEntries(datos.entries());
-            //const newDatosJson=JSON.stringify(datosCompletos);
-            form.submit();
-        });
-	}
-    else{
+	var valido=form.checkValidity();
+	if(valido){
+		alert("SE A ENVIADO PERFECTAMENTE");
+		form.submit();
+	}else{
 		alert("Los campos no son validos");
 	}
 }
 
 window.addEventListener("load", iniciar);
+
 
 
 
